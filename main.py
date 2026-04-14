a428e6ec818589afd882994f33063b7412faf69d
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-04-14 21:18:08 +0530

    Refactor salary calculation prompts and outputs

--- main.py
+++ main.py
@@ -3,15 +3,17 @@
 # Coder: Shamal
 # Date: 26/1/2026
 # Class: ECS/E2
-print("Salary  Details:")
-BS = float(input("Basic Salary:5000 "))
-DA = 0.7*BS
-TA = 0.3*BS
-HRA = 0.1*BS
-GS = BS+DA+TA+HRA
-print(f"DA(70%): {DA: .1f}")
-print(f"TA(30%): {TA: .1f}")
-print(f"HRA(10%): {HRA: .1f}")
-print(f"Gross Salary: {GS: .1f}")
+print("Salary Calculator")
+BS = float(input("Enter the basic salary: "))
 
+DA = 0.7 * BS
+TA = 0.3 * BS
+HRA = 0.1 * BS
+GS = BS + DA + TA + HRA
 
+print("\nSalary Details:")
+print(f"Basic Salary:\t{BS}")
+print(f"DA (70%):\t{DA}")
+print(f"TA (30%):\t{TA}")
+print(f"HRA (10%):\t{HRA}")
+print(f"Gross Salary:\t{GS}")
